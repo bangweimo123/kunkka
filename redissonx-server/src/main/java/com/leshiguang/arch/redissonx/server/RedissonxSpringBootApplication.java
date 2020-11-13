70f3618f85073491a7366a5df11398ce36ca61f8
bangwei.mo <bangwei.mo@lifesense.com>
2020-11-13 10:32:52 +0800

    迁移cas依赖

--- redissonx-server/src/main/java/com/leshiguang/arch/redissonx/server/RedissonxSpringBootApplication.java
+++ redissonx-server/src/main/java/com/leshiguang/arch/redissonx/server/RedissonxSpringBootApplication.java
@@ -1,6 +1,7 @@
 package com.leshiguang.arch.redissonx.server;
 
 import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
+import com.leshiguang.arch.cas.support.springboot.EnableCasClient;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,13 +16,16 @@ import org.springframework.scheduling.annotation.EnableScheduling;
  */
 @Configuration
 @EnableScheduling
-@EnableApolloConfig({"application", "lx-arch.redis", "lx-arch.redissonx", "lx-arch.cas"})
+@EnableApolloConfig({"application", "lx-arch.redissonx"})
 @SpringBootApplication(scanBasePackages = {"com.leshiguang.arch.cas", "com.leshiguang.arch.redissonx"}, exclude = {TaskExecutionAutoConfiguration.class, // 禁用定时任务
 })
 @MapperScan({"com.leshiguang.arch.redissonx.core.mapper.gen", "com.leshiguang.arch.redissonx.core.mapper.ext"})
+@EnableCasClient
 public class RedissonxSpringBootApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RedissonxSpringBootApplication.class, args);
     }
+
+
 }
--- redissonx-server/src/main/java/com/leshiguang/arch/redissonx/server/controller/common/IndexController.java
+++ redissonx-server/src/main/java/com/leshiguang/arch/redissonx/server/controller/common/IndexController.java
@@ -1,6 +1,5 @@
 package com.leshiguang.arch.redissonx.server.controller.common;
 
-import com.leshiguang.arch.cas.support.service.LogoutService;
 import com.leshiguang.redissonx.common.base.RedissonxResponse;
 import com.leshiguang.redissonx.common.base.RedissonxResponseBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,8 +7,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
@@ -20,8 +17,6 @@ import java.io.IOException;
  */
 @RestController
 public class IndexController {
-    @Resource
-    private LogoutService logoutService;
 
     @GetMapping(value = "/")
     public void index(HttpServletResponse res) {
@@ -36,10 +31,5 @@ public class IndexController {
     @ResponseBody
     public RedissonxResponse echo(@RequestParam(required = false) String requestId) {
         return RedissonxResponseBuilder.success(requestId);
-    }
-
-    @GetMapping(value = "/logout")
-    public void logout(HttpServletRequest req, HttpServletResponse res) {
-        logoutService.logout(req, res);
     }
 }
